e5158e9e4c702af3a93aca475b8c5ad2bdee9c65
yusufva <yusufvalent@gmail.com>
2023-04-18 00:18:41 +0700

    modify entity

--- entity/comment.go
+++ entity/comment.go
@@ -8,9 +8,9 @@ import (
 
 type Comment struct {
 	ID        int            `gorm:"primaryKey;not null" json:"id"`
-	UserID    int            `gorm:"not null;foreignKey;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"user_id"`
-	PhotoID   int            `gorm:"not null;foreignKey;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"photo_id"`
-	Message   int            `gorm:"not null;type:text" json:"message"`
+	UserID    int            `gorm:"not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"user_id"`
+	PhotoID   int            `gorm:"not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"photo_id"`
+	Message   string         `gorm:"not null;type:text" json:"message"`
 	CreatedAt time.Time      `gorm:"default:now()" json:"created_at"`
 	UpdatedAt time.Time      `gorm:"default:now()" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
