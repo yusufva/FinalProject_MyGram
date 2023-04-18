1e550cfbdc62e69feac4f17483ed633b49d47eb9
yusufva <yusufvalent@gmail.com>
2023-04-19 01:56:36 +0700

    fixing validation and adding new request

--- dto/user.go
+++ dto/user.go
@@ -1,10 +1,10 @@
 package dto
 
 type NewUserRequest struct {
-	Username string `json:"username" valid:"required~username cannot be empty"`
+	Username string `json:"username" valid:"required~username cannot be empty,alphanum"`
 	Email    string `json:"email" valid:"required~email cannot be empty,email"`
 	Password string `json:"password" valid:"required~password cannot be empty,stringlength(6|72)"`
-	Age      int    `json:"age" valid:"required~age cannot be empty,min=8"`
+	Age      int    `json:"age" valid:"required~age cannot be empty,range(8|100)"`
 }
 
 type NewUserResponse struct {
@@ -15,6 +15,11 @@ type NewUserResponse struct {
 
 type TokenResponse struct {
 	Token string `json:"token"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username" valid:"required~username cannot be empty,alphanum"`
+	Password string `json:"password" valid:"required~password cannot be empty,stringlength(6|72)"`
 }
 
 type LoginResponse struct {
